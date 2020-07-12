b5c6535cf6d92f5efd79799a9f9ea5bfe69bcdd9
Tiago Santos <pereirat476@gmail.com>
2020-07-12 16:22:01 -0300

    Alteração do local das pastas de trabalho

---
+++ eventoapp/src/main/java/Controllers/EventoController.java
@@ -0,0 +1,12 @@
+package Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventoController {
+	@RequestMapping("/cadastraEvento")
+	public String form() {
+		return "formEvento";
+	}
+}
