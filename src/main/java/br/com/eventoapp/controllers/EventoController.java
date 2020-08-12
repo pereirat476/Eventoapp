cb5db6ab4b55872b930745bb3f9dab956162c188
Tiago Santos <pereirat476@gmail.com>
2020-08-12 11:02:42 -0300

    Cadastrando convidados no Evento

--- src/main/java/br/com/eventoapp/controllers/EventoController.java
+++ src/main/java/br/com/eventoapp/controllers/EventoController.java
@@ -1,12 +1,61 @@
 package br.com.eventoapp.controllers;
 
+import br.com.eventoapp.models.Convidado;
+import br.com.eventoapp.models.Evento;
+import br.com.eventoapp.repository.ConvidadoRepository;
+import br.com.eventoapp.repository.EventoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EventoController {
-	@RequestMapping("/cadastraEvento")
+
+	@Autowired
+	EventoRepository er;
+
+	@Autowired
+	ConvidadoRepository cr;
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
 	public String form() {
 		return "evento/formEvento";
 	}
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
+	public String form(Evento evento) {
+
+		er.save(evento);
+		return "redirect:/cadastrarEvento";
+	}
+
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
+	}
+
+	
+
+	@RequestMapping(value = "/{codigo}",method=RequestMethod.GET)
+	public ModelAndView detalhesEvento(@PathVariable("codigo")long codigo) {
+	Evento evento = er.findByCodigo(codigo);
+	ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+	mv.addObject("evento",evento);
+	return mv;
+	}
+
+	@RequestMapping(value = "/{codigo}",method=RequestMethod.POST)
+	public String detalhesEvento(@PathVariable("codigo")long codigo, Convidado convidado) {
+		Evento evento = er.findByCodigo(codigo);
+		convidado.setEvento(evento);
+		cr.save(convidado);
+		return "redirect:/{codigo}";
+	}
+	
 }
--- src/main/java/br/com/eventoapp/models/Evento.java
+++ src/main/java/br/com/eventoapp/models/Evento.java
@@ -1,11 +1,33 @@
 package br.com.eventoapp.models;
 
-public class Evento {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+public class Evento implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long codigo;
+	
 
 	private String nome;
 	private String local;
 	private String data;
 	private String horario;
+
+	@OneToMany
+	private List<Convidado> convidados;
+	
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
 
 	public String getNome() {
 		return nome;
